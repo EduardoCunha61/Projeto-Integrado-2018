a33ce1383c57a2c4b10dcd7ddfe0cfa5e4fa519e
Hodor61 <eduardodelimacunha@gmail.com>
2018-04-13 16:09:05 +0100

    Correções na validação
    Programa passa a imprimir todos os erros ao mesmo tempo.
    Reconhcimento de mais erros na validação (por exemplo no fim de ficheiro a analisar)

--- Validate_ExercDSL.py
+++ Validate_ExercDSL.py
@@ -1,51 +1,65 @@
 import re
 
 #TRATAR DOS ACENTOS!!!!
-#ALTERAR VALORES DE RETORNO DA VALIDAÇÃO. TALVEZ FOSSE MELHOR IMPRIMIR VÁRIOS VALORES?
-#ADICIONAR END OF FILE DEPOIS DE RECONHECER O '.'
+#INDEX DA LISTA AO VERIFICAR PODE DAR ERRO (CASO NÃO SEJAM DIVIDIDAS EM 2 LINHAS (neste caso))
 
 #PRÓXIMA TAREFA: IR BUSCAR OS VALORES DE TESTE PARA VALIDAR FUNÇÃO
 
 # Separa ficheiro de entrada por linhas, sendo dividido por '},\n'.
 # É adicionado o caractér '}' no final de cada linha não vazia e que não termina em '.'
-# Termina a execução da função quando encontra um '.' no final de uma linha (linha final)
 def split_file(file_exerc):
     new_lines=[]
+    delim_final = "}."
+    flag = 0
     with open(file_exerc) as f:
-        for line in f.read().split('},\n'):
+        split_lines = f.read().split('},\n')
+        for line in split_lines:
+            end_of_file = re.findall("}\.",line)
+
+            if end_of_file:
+                aux = line.split(delim_final)
+                line = aux[0] + delim_final
+                if aux[1]:
+                    flag = 2
             if not line.endswith(".") or not line:
                 new_lines.append((line+'}'))
             if line.endswith("."):
                 new_lines.append(line[:-1])
-                # END OF FILE AQUI
-    return new_lines
+    if not end_of_file:
+        flag = 1
+    return (new_lines,flag)
 
 # Valida todos os campos do ficheiro de entrada de acordo com a estrutura definida no ficheiro "teste.txt"
 # Esta função tem que ser optimizada, visto que, só retorna um erro de cada vez (Era fixe retornar todos)
 def valida_Exerc(file_ex):
-    split_lines = split_file(file_ex) 
+    split_lines,flag = split_file(file_ex)
+    res = []
 
-    if (bool(re.match('Enunciado: *{"[^"]*"}', split_lines[0]))):
-        if(bool(re.match('Template: *{".*"\t*}', split_lines[1], re.DOTALL))):           # É preciso rever esta expressão regular (visto que strings estão entre "")!                               
-            if (bool(re.match('Valores de teste: *{(\("[^"]*","[^"]*"\),?)+}', split_lines[2]))):
-                print(split_lines[2])
-                if(bool(re.match('Linguagem: *{"[a-zA-Z]*"}', split_lines[3]))):
-                    return 1
-                else:
-                    return -1
-            else:        
-                return -2
-        else:
-            return -3
-    else:
-        return -4
+    if flag == 1:
+        res.append(-6)
+    elif flag == 2:
+        res.append(-5)
+
+    if (not bool(re.match('Enunciado: *{"[^"]*"}', split_lines[0]))):
+        res.append(-4)
+
+    if(not bool(re.match('Template: *{".*"\t*}', split_lines[1], re.DOTALL))):
+        res.append(-3)
+
+    if (not bool(re.match('Valores de teste: *{(\("[^"]*","[^"]*"\),?)+}', split_lines[2]))):
+        res.append(-2)              
+                
+    if(not bool(re.match('Linguagem: *{"[a-zA-Z]*"}', split_lines[3]))):
+        res.append(-1)
+
+    return res
 
 # Cria uma matriz 2 por numero de campos, sendo que guarda a info neste formato:
 # [["Numero Aluno","a71940"],["Resposta","int factorial(int x){int res = 1;...}"]
 # Todas as chavetas usadas para delimitar cada campo são removidas
 
 def check_lines(file_exec):
-    new_lines = split_file(file_exec)
+    new_lines = split_file(file_exec)[0]
     largura, altura = 2, len(new_lines)
     matrix = [["" for x in range(largura)] for y in range(altura)]
 
@@ -83,23 +97,34 @@ def remove_chavetas(stri):
 def criarJSON(matriz_linhas):
     f = open("testeaux.json","w+")
     f.write("{\"exercicio\": {\n")
+
     for line in matriz_linhas:
-        f.write('\t' + line[0] + " :" + line[1] + ",\n\n")
+        if line==matriz_linhas[3]:
+            f.write('\t' + line[0] + " :" + line[1] + "\n")
+        else:
+            f.write('\t' + line[0] + " :" + line[1] + ",\n\n")
     f.write("}}")
     f.close()
 
 if __name__ == '__main__':
+
     res = valida_Exerc('teste.txt')
 
-    if(res==1):
-        print("Ficheiro corretamente criado!")
+    if not res:
+        print("Ficheiro correto!")
         criarJSON(check_lines('teste.txt'))
 
-    elif(res==-1):
-        print("LINGUAGEM INVALIDA")
-    elif(res==-2):
-        print("VALORES DE TESTE INVALIDOS")
-    elif(res==-3):
-        print("TEMPLATE INVALIDO")
-    elif(res==-4):
-        print("ENUNCIADO INVALIDO")
+    for num in reversed(res):
+        print(num)
+        if(num==-1):
+            print("LINGUAGEM INVALIDA")
+        elif(num==-2):
+            print("VALORES DE TESTE INVALIDOS")
+        elif(num==-3):
+            print("TEMPLATE INVALIDO")
+        elif(num==-4):
+            print("ENUNCIADO INVALIDO")
+        elif(num==-5):
+            print("Existe codigo posterior a estrutura do ficheiro correta!")
+        elif(num==-6):
+            print("Ficheiro nao terminado como esperado! ('}.')")
--- Validate_SubmDSL.py
+++ Validate_SubmDSL.py
@@ -1,41 +1,64 @@
 import re
 
 #TRATAR DOS ACENTOS!!!!
-#ALTERAR VALORES DE RETORNO DA VALIDAÇÃO. TALVEZ FOSSE MELHOR IMPRIMIR VÁRIOS VALORES?
-#ADICIONAR END OF FILE DEPOIS DE RECONHECER O '.'
+#INDEX DA LISTA AO VERIFICAR PODE DAR ERRO (CASO NÃO SEJAM DIVIDIDAS EM 2 LINHAS (neste caso))
 
 #PRÓXIMA TAREFA: IR BUSCAR NUMERO DE ALUNO E ID DO EXERCICIO PARA GERAR RESULTADO
 
+# Separa ficheiro de entrada por linhas, sendo dividido por '},\n'.
+# É adicionado o caractér '}' no final de cada linha não vazia e que não termina em '.
 def split_file(file_subm):
     new_lines=[]
-
+    delim_final = "}."
+    flag = 0
+    
     with open(file_subm) as f:
-        for line in f.read().split('},\n'):
+        split_lines = f.read().split('},\n')
+
+        for line in split_lines:
+            end_of_file = re.findall("}\.",line)
+
+            if end_of_file:
+                aux = line.split(delim_final)
+                line = aux[0] + delim_final
+                if aux[1]:
+                    flag = 5
+
             if not line.endswith(".") or not line:
                 new_lines.append((line+'}'))
+
             if line.endswith("."):
                 new_lines.append(line[:-1])
-                # END OF FILE AQUI
-        
-    return new_lines
-   
-def valida_Subm(file_subm):
-    split_lines = split_file(file_subm) 
 
-    if(bool(re.match('[nN]umero [aA]luno: *{"[a-zA-Z]?[0-9]{5}"}', split_lines[0]))):                                       
-        if (bool(re.match('[rR]esposta: *{".*"\t*}.', split_lines[1], re.DOTALL))): # É preciso rever esta expressão regular (Se adicionarmos um \n ou \t no ficheiro de entrada, dá erro!)
-            return 1
-        else:
-            return -1
-    else:
-        return -2
+    if not end_of_file:
+        flag = 1
+    return (new_lines,flag)
+
+# Valida todos os campos do ficheiro de entrada de acordo com a estrutura definida no ficheiro "plain.txt"
+# Esta função tem que ser optimizada, visto que, só retorna um erro de cada vez (Era fixe retornar todos)
+def valida_Subm(file_subm):
+    split_lines,flag = split_file(file_subm) 
+    res = []
+
+    if flag == 1:
+        res.append(-6)
+    elif flag == 5:
+        res.append(-5)
+
+    if(not bool(re.match('[nN]umero [aA]luno: *{"[a-zA-Z]?[0-9]{5}"}', split_lines[0]))):                                       
+        res.append(-2)
+    
+    if (not bool(re.match('[rR]esposta: *{\".*\"\t*}', split_lines[1], re.DOTALL))):
+        res.append(-1)
+    
+    return res
 
 # Cria uma matriz 2 por numero de campos, sendo que guarda a info neste formato:
 # [["Enunciado","Cria o fatorial de um dado inteiro X"],["Template","ASD"],etc]
 # Todas as chavetas usadas para delimitar cada campo são removidas
 
 def check_lines(file_subm):
-    new_lines = split_file(file_subm)
+    new_lines = split_file(file_subm)[0]
     largura, altura = 2, len(new_lines)
     matrix = [["" for x in range(largura)] for y in range(altura)]
 
@@ -58,21 +81,31 @@ def remove_chavetas(stri):
 # Cria um ficheiro JSON com o formato de subm.json
 def criarJSON(matriz_linhas):
     f = open("submteste.json","w+")
-    f.write("{\"submissao\": {\n")
+    f.write("{\"exercicio\": {\n")
+
     for line in matriz_linhas:
-        f.write('\t' + line[0] + " :" + line[1] + ",\n\n")
+        if line==matriz_linhas[1]:
+            f.write('\t' + line[0] + " :" + line[1] + "\n")
+        else:
+            f.write('\t' + line[0] + " :" + line[1] + ",\n\n")
     f.write("}}")
     f.close()
 
-
 if __name__ == '__main__':
+
     res = valida_Subm('plain.txt')
-    
-    if(res==1):
-        print("Ficheiro corretamente criado!")
+
+    if not res:
+        print("Ficheiro correto!")
         criarJSON(check_lines('plain.txt'))
 
-    elif(res==-1):
-        print("RESPOSTA INVALIDA")
-    elif(res==-2):
-        print("NUMERO DE ALUNO INVALIDO")
+    for num in reversed(res):
+        print(num)
+        if(num==-1):
+            print("RESPOSTA INVALIDA")
+        elif(num==-2):
+            print("NUMERO DE ALUNO INVALIDO")
+        elif(num==-5):
+            print("Existe codigo posterior ao fim de ficheiro analisado!")
+        elif(num==-6):
+            print("Ficheiro nao terminado como esperado! ('}.')")
